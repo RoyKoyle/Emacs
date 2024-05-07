bba399becedaa4406a382a865a1cb10c7f7d689b
Roy A. Koyle <eckertdawid@gmail.com>
2024-05-07 23:40:11 +0200

    cleanup

--- init.el
+++ init.el
@@ -1,27 +1,4 @@
-;;; This fixed garbage collection, makes emacs start up faster ;;;;;;;
-(setq gc-cons-threshold 402653184
-      gc-cons-percentage 0.6)
-
-(defvar startup/file-name-handler-alist file-name-handler-alist)
-(setq file-name-handler-alist nil)
-
-(defun startup/revert-file-name-handler-alist ()
-  (setq file-name-handler-alist startup/file-name-handler-alist))
-
-(defun startup/reset-gc ()
-  (setq gc-cons-threshold 16777216
-	gc-cons-percentage 0.1))
-
-(add-hook 'emacs-startup-hook 'startup/revert-file-name-handler-alist)
-(add-hook 'emacs-startup-hook 'startup/reset-gc)
-
-(setq inhibit-startup-message t)
-
-(setq locale-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
+;; init point
 
 (when (file-readable-p "~/.emacs.d/config.org")
   (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
