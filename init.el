e88a891980a2fb4362434d723f5656e9d12b3f16
Roy A. Koyle <eckertdawid@gmail.com>
2024-05-08 00:02:23 +0200

    furhter stuff

--- init.el
+++ init.el
@@ -8,7 +8,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(magit popup-kill-ring rainbow-delimiters beacon hungry-delete avy spaceline dashboard projectile helm swiper switch-window yasnippet-snippets yasnippet which-key cyberpunk-theme afternoon-theme)))
+   '(company flycheck diminish org-bullets magit popup-kill-ring rainbow-delimiters beacon hungry-delete avy spaceline dashboard projectile helm swiper switch-window yasnippet-snippets yasnippet which-key cyberpunk-theme afternoon-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
